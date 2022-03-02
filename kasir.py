4047d36048f2fd0e1398e6189993c74efea0e719
joshuajevon <joshuajevon05@gmail.com>
2022-03-02 09:36:33 +0700

    update

--- kasir.py
+++ kasir.py
@@ -6,36 +6,36 @@ print("3. Soto Ayam = Rp10.000")
 print("4. Nasi Padang = Rp25.000")
 print("5. Teh = Rp5.000")
 
-pilihan = input("Pilih Menu:")
-if pilihan == "1":
+pilihan = int(input("Pilih Menu:"))
+if pilihan == 1:
     pilihan1 = int(input("Jumlah porsi nasi goreng:"))
     totalHarga = pilihan1 * 20000
-    print("Total Harga:",totalHarga)
-    uang = int(input("Uang yang dibayarkan:"))
+    print("Total Harga:Rp",totalHarga)
+    uang = int(input("Uang yang dibayarkan:Rp"))
     kembalian = uang - totalHarga
-    print("Uang kembalian:",kembalian)
-elif pilihan == "2":
+    print("Uang kembalian:Rp",kembalian)
+elif pilihan == 2:
     pilihan1 = int(input("Jumlah porsi bakso:"))
     totalHarga = pilihan1 * 15000
     print("Total Harga:Rp",totalHarga)
     uang = int(input("Uang yang dibayarkan:Rp"))
     kembalian = uang - totalHarga
     print("Uang kembalian:Rp",kembalian)
-elif pilihan == "3":
+elif pilihan == 3:
     pilihan1 = int(input("Jumlah porsi soto ayam:"))
     totalHarga = pilihan1 * 10000
     print("Total Harga:Rp",totalHarga)
     uang = int(input("Uang yang dibayarkan:Rp"))
     kembalian = uang - totalHarga
     print("Uang kembalian:Rp",kembalian)
-elif pilihan == "4":
+elif pilihan == 4:
     pilihan1 = int(input("Jumlah porsi nasi padang:"))
     totalHarga = pilihan1 * 25000
     print("Total Harga:Rp",totalHarga)
     uang = int(input("Uang yang dibayarkan:Rp"))
     kembalian = uang - totalHarga
     print("Uang kembalian:Rp",kembalian)
-elif pilihan == "5":
+elif pilihan == 5:
     pilihan1 = int(input("Jumlah teh:"))
     totalHarga = pilihan1 * 5000
     print("Total Harga:Rp",totalHarga)
